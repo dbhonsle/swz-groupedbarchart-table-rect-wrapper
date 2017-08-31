e4aafab2233b541204c79b83fe829f5985563f93
Deepak <dbhonsle@rewdale.com>
2017-08-31 17:11:11 +0530

    First commit

---
+++ src/lib/components/D3ChartwithTableWrapper/D3ChartwithTableWrapper.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import d3Chart  from 'swz-groupedbarchart-with-table';
+import './D3ChartwithTableWrapper.css';
+
+export default class D3ChartwithTableWrapper extends Component {
+    constructor(props) {
+        super(props)
+        this.chart = new d3Chart(props);
+    }
+
+    // When the DOM is ready, create the chart.
+    componentDidMount() {
+        let el = ReactDOM.findDOMNode(this);
+        this.chart.create(el, {
+            width: this.props.width || el.offsetWidth,
+            height: this.props.height || el.offsetHeight,
+            barChartXOffset: this.props.barChartXOffset || 0.15,
+            barChartHeight: this.props.barChartHeight || 0.666
+            }, this.getChartState() );
+    }
+
+    // Update the Chart
+    componentDidUpdate(prevProps, prevState) {
+        let el = ReactDOM.findDOMNode(this);
+        this.chart.update(el, this.getChartState());
+    }
+
+    //Destroy chart before unmount.
+    componentWillUnmount() {
+        let el = ReactDOM.findDOMNode(this);
+        this.chart.destroy(el);
+    }
+
+    //Create the div which the chart will be rendered to.
+    render() {
+        return (
+            <div className="Chart-bar-table" style={{width: this.props.width, height: this.props.height}}></div>
+        );
+    }
+
+    getChartState() {
+    return {
+      data: this.props.data
+    };
+  } 
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+  context: path.resolve(__dirname, './src/lib'),
+  entry: {
+    D3ChartwithTableWrapper: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { 
+            plugins: [
+              "transform-class-properties",
+              "transform-object-rest-spread",
+              "transform-decorators-legacy"
+            ],
+            presets: [
+              ['es2015', { 'modules': false }],
+              "es2016",
+              "react"
+            ]
+          },
+        }],
+      },
+	    
+      // Loaders for other file types can go here
+	{
+        test: /\.(sass|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      } 
+    ],
+  }
+};
