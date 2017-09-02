0a6241977ebc1b04d1f0300243cd23b316258b8c
Deepak <dbhonsle@rewdale.com>
2017-09-02 17:27:06 +0530

    changed to module for EMS

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.umd.js',
     libraryTarget: "umd"
   },
   module: {
